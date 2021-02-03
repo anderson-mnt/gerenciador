2e33dc65297004099c733ba8bba41a4153317856
Anderson Monteiro <anderson.a.monteiro@outlook.com.br>
2021-02-03 13:22:59 -0300

    Curso Alura - Java Servlets - Primeira Servlet - Fundamentos da Web e a API de Servlet

---
+++ src/br/com/alura/gerenciador/servlet/FirstServlet.java
@@ -0,0 +1,33 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns="/PrimeiraServlet")
+public class FirstServlet extends HttpServlet{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Primeira Servlet");
+		out.println("</body>");
+		out.println("</html>");
+		
+		System.out.print("O primeiro Servlet foi chamado");
+	}
+
+}
