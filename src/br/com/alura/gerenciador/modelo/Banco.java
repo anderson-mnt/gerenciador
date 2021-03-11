e60c02c7c4309393404e431275681aa6c502175b
anderson-mnt <anderson.a.monteiro@outlook.com.br>
2021-03-10 23:12:45 -0300

    Curso Alura-Servlet II - Formulario de login

--- src/br/com/alura/gerenciador/modelo/Banco.java
+++ src/br/com/alura/gerenciador/modelo/Banco.java
@@ -7,7 +7,32 @@ import java.util.List;
 public class Banco {
 
 	private static List<Empresa> lista = new ArrayList<>();
+	private static List<Usuario> listaUsuarios = new ArrayList<>();
 	private static Integer chaveSequencial = 1;
+	
+	static {
+		Empresa empresa = new Empresa();
+		empresa.setId(chaveSequencial++);
+		empresa.setNome("Alura");
+		Empresa empresa2 = new Empresa();
+		empresa.setId(chaveSequencial++);
+		empresa.setNome("Caelum");
+		lista.add(empresa);
+		lista.add(empresa2);
+		
+		Usuario u1 = new Usuario();
+		u1.setLogin("admin");
+		u1.setSenha("admin");
+		
+
+		Usuario u2 = new Usuario();
+		u2.setLogin("adm");
+		u2.setSenha("adm");
+		
+		listaUsuarios.add(u1);
+		listaUsuarios.add(u2);
+		
+	}
 	
 	public void adiciona(Empresa empresa) {
 		empresa.setId(Banco.chaveSequencial++);
@@ -41,5 +66,14 @@ public class Banco {
 		}
 		return null;
 		
+	}
+
+	public Usuario existeUsuario(String login, String senha) {
+		for(Usuario usuario : listaUsuarios) {
+			if(usuario.ehIgual(login, senha)) {
+				return usuario;
+			}
+		}
+		return null;
 	}
 }
